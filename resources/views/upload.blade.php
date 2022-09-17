0fcf46005d7eb980c0ccf2b8b82daec683cdff78
MaekawaMoikako <maekawamikako@gmail.com>
2022-09-17 14:23:55 +0900

    add: upload画面

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,1 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/upload.css">
+    <script type="text/javascript" src="js/upload.js"></script>
+</head>
+<body>
+    <div class="upload-container">
+        <div class="uploader">
+            <input type="file" name="file" />
+        </div>
+        <div class="forms-cntainer">
+            <div class="forms">
+                <select name="project" required>
+                    <option value="">プロジェクトを選択</option>
+                    <option value="test">テスト</option>
+                </select>
+                <input class="date" type="date" name="example1">
+            </div>
+            <button type="button">決定</button>
+        </div>
+    </div>
+</body>
+</html>
