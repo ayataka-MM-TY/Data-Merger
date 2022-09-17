0b37311752793852cb034f9ce1c69a7a3df5fe03
MaekawaMoikako <maekawamikako@gmail.com>
2022-09-17 15:10:54 +0900

    add: download画面の見た目

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/download.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <th>プロジェクト名</th> <th>データ件数</th> <th>最終更新</th>
+        </tr>
+        <tr>
+            <td>プロジェクト</td> <td>5件</td> <td>2022/09/17</td>
+        </tr>
+    </table>
+</body>
+</html>
