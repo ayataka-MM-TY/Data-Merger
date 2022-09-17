34dc4d792d08d70c148864a60e3d757bf80d1c61
MaekawaMoikako <maekawamikako@gmail.com>
2022-09-17 20:37:14 +0900

    add: confirm画面の見た目

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -1,1 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/confirm.css">
+</head>
+<body>
+    <div class="confirm-container">
+        <p>{{ $filename }}を{{ $project }}に保存しますか？</p>
+        <form action="/" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="buttons">
+                <button type="submmit">キャンセル</button>
+                <button type="submmit">保存</button>
+            </div>
+        </form>
+        <table>
+            <tr>
+                @foreach($titles as $title)
+                    <th>{{ $title }}</th>
+                @endforeach
+            </tr>
+            @foreach($records as $record)
+                <tr>
+                    @foreach($record as $value)
+                        <td>{{ $value }}</td>
+                    @endforeach
+                </tr>
+            @endforeach
+        </table>
+    </div>
+</body>
+</html>
