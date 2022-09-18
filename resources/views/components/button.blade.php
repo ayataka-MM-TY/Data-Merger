8a69103e9e055bc563a4ef2378d5b91ec876343f
MaekawaMoikako <maekawamikako@gmail.com>
2022-09-18 10:17:42 +0900

    add: ボタンのコンポーネント化

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,11 @@
+<style>
+  button {
+  padding: 10px 36px;
+  border: 1px solid #848484;
+  border-radius: 8px;
+  background-color: #fff;
+  cursor: pointer;
+}
+</style>
+
+<button type="submit">{{ $text }}</button>
